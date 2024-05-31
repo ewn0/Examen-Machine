efd8a70f8f0a2f2dc093a174ed15bab07669731e
ewn0 <ewan.leneillon@gmail.com>
2024-05-31 11:50:09 +0200

    Ajout de la fonction AfficherTableau

--- Examen Machine/Program.cs
+++ Examen Machine/Program.cs
@@ -6,9 +6,17 @@ public class Program
     {
         Console.WriteLine("Saisir le nombre d'éléments harmoniques");
         int Taille = Convert.ToInt32(Console.ReadLine());
+        /*if (Taille! > 0)
+        {
+            throw new Exception("Le nombre d'éléments doit être un nombre supérieur à 0.");
+        }*/
         int[] tableau = new int[Taille];
         return tableau;
+
+    }
+    public static void AfficherTableau(int[] tableau)
+    {
+        Console.WriteLine("Le tableau résultat contient :");
+        foreach (int valeur in tableau) { Console.WriteLine(valeur + " "); }
     }
 }
-
-
