f4095278e7e66ebe66833e6099d29a004a6184ec
ewn0 <ewan.leneillon@gmail.com>
2024-05-31 11:42:17 +0200

    Création d'un tableau vide de taille demandée par l'utilisateur.

---
+++ Examen Machine/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Program
+{
+    public static int[] CreerTableau()
+    {
+        Console.WriteLine("Saisir le nombre d'éléments harmoniques");
+        int Taille = Convert.ToInt32(Console.ReadLine());
+        int[] tableau = new int[Taille];
+        return tableau;
+    }
+}
+
+
