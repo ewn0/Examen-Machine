f131449641f601415dafe9dbaf1fd39e8850f154
ewn0 <ewan.leneillon@gmail.com>
2024-05-31 12:15:24 +0200

    Ajout D'un main, correction du système de gestion des exceptions ainsi que de la fonction Calculer_Harmoniques

--- Examen Machine/Program.cs
+++ Examen Machine/Program.cs
@@ -1,22 +1,49 @@
 ﻿using System;
+using System.Linq.Expressions;
 
 public class Program
 {
     public static int[] CreerTableau()
     {
         Console.WriteLine("Saisir le nombre d'éléments harmoniques");
-        int Taille = Convert.ToInt32(Console.ReadLine());
-        /*if (Taille! > 0)
+        try
         {
-            throw new Exception("Le nombre d'éléments doit être un nombre supérieur à 0.");
-        }*/
-        int[] tableau = new int[Taille];
-        return tableau;
-
+            int Taille = Convert.ToInt32(Console.ReadLine());
+            int[] tableau = new int[Taille];
+            return tableau;
+        } 
+        
+        catch(Exception e)
+        {
+            Console.WriteLine($"Erreur {e.Message}");
+            return [];
+        }
+        
     }
     public static void AfficherTableau(int[] tableau)
     {
         Console.WriteLine("Le tableau résultat contient :");
         foreach (int valeur in tableau) { Console.WriteLine(valeur + " "); }
+    }
+
+    public static void Calcul_Harmonique(int[] tableau)
+    {
+        tableau[0] = 1;
+        for(int valeur = 1; valeur < tableau.Length; valeur++)
+        {
+            tableau[valeur] = tableau[valeur - 1] + (1/valeur+1);
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] tab;
+        try
+        {
+            tab = CreerTableau();
+            Calcul_Harmonique(tab);
+            AfficherTableau(tab);
+        } catch(Exception e) { Console.WriteLine(e.ToString()); }
+        
     }
 }
